d2d2bba96e7672d3bb4b8fea6c496b4f463fbea5
mh.hong <mh.hong@woowahan.com>
2024-07-07 00:21:31 +0900

    Update ChannelApiControllerTest.java

--- src/test/java/com/example/mytv/adapter/in/api/ChannelApiControllerTest.java
+++ src/test/java/com/example/mytv/adapter/in/api/ChannelApiControllerTest.java
@@ -15,7 +15,6 @@ import com.example.mytv.domain.channel.ChannelFixtures;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -37,7 +36,7 @@ class ChannelApiControllerTest {
     @DisplayName("POST /api/v1/channels")
     void testCreateChannel() throws Exception {
         // Given
-        var channelRequest = new ChannelRequest(new ChannelSnippetRequest("title", "description", "https://example.com/thumbnail,jpg"), "userId");
+        var channelRequest = new ChannelRequest(new ChannelSnippetRequest("title", "description", "https://example.com/thumbnail.jpg"), "userId");
         given(channelUseCase.createChannel(any())).willReturn(ChannelFixtures.stub("channelId"));
 
         // When
