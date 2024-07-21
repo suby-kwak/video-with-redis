f8c9c5728fb94bc4ac003e4cacc736bc8c26e7ab
mh.hong <kielhong@gmail.com>
2024-07-21 17:01:56 +0900

    feat : local 실행환경

---
+++ src/main/java/com/example/mytv/adapter/in/api/dto/CommandResponse.java
@@ -0,0 +1,10 @@
+package com.example.mytv.adapter.in.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class CommandResponse {
+    private String id;
+}
