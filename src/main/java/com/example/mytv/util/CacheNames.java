8c614c2f14a6be890c81ab3778fe24f33d0e9117
mh.hong <kielhong@gmail.com>
2024-07-24 18:24:05 +0900

    feat : authKey 로 User 조회 처리

--- src/main/java/com/example/mytv/util/CacheNames.java
+++ src/main/java/com/example/mytv/util/CacheNames.java
@@ -3,6 +3,7 @@ package com.example.mytv.util;
 public class CacheNames {
     public static final String SEPARATOR = ":";
 
+    public static final String USER_SESSION = "user-session";
     public static final String CHANNEL = "channel";
     public static final String VIDEO = "video";
     public static final String VIDEO_LIST = VIDEO + SEPARATOR + "list";
--- src/main/java/com/example/mytv/util/RedisKeyGenerator.java
+++ src/main/java/com/example/mytv/util/RedisKeyGenerator.java
@@ -1,10 +1,14 @@
 package com.example.mytv.util;
 
 import static com.example.mytv.util.CacheNames.SEPARATOR;
+import static com.example.mytv.util.CacheNames.USER_SESSION;
 import static com.example.mytv.util.CacheNames.VIDEO_LIKE;
 import static com.example.mytv.util.CacheNames.VIDEO_VIEW_COUNT;
 
 public class RedisKeyGenerator {
+    public static String getUserSessionKey(String authKey) {
+        return USER_SESSION + SEPARATOR + authKey;
+    }
     public static String getVideoViewCountKey(String videoId) {
         return VIDEO_VIEW_COUNT + SEPARATOR +  videoId;
     }
